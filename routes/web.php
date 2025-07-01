3337be8a26f15215d1b70398a0f3cec9118df3f9
Akmal <akmalthemalong@gmail.com>
2025-07-01 19:01:11 +0800

    chore(web): Hapus rute default '/'

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/nota/{project}/cetak', [NotaController::class, 'cetak'])->name('nota');
